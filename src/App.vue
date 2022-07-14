8341aa4139f33facc7f1f1e935f92132384a1c62
vincaf <vincenzocafassog@gmail.com>
2022-07-14 17:05:16 +0200

    added JumboTron component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <JumboTron />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JumboTron from './components/JumboTron.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JumboTron
   }
 }
 </script>
---
+++ src/components/JumboTron.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="jumbo">
+    <h1>Il mio Jumbo Vue</h1>
+    <p>spero di averlo fatto bene</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
